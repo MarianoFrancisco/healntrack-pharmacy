b2f55b622a0521dd2cd7d52c9035c951a977e1c5
Mariano Camposeco <mariano1941@outlook.es>
2025-10-23 17:14:59 -0600

    chore: add project structure

---
+++ pharmacy/src/test/java/com/sa/healntrack/pharmacy/PharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sa.healntrack.pharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
